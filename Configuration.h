fe7cf3e85b6bb58466e9e7add1447a1f41f520fc
Peter Blicharski <peter.blicharski@gmail.com>
2015-05-23 23:15:59 +0200

    Update steps per unis for new Z-axis upgrade

--- Configuration.h
+++ Configuration.h
@@ -293,7 +293,7 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {64.25,64.25,2560,600}  // default steps per unit for ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {64.25,64.25,2133.333,600}  // default steps per unit for ultimaker
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 500}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
